c5d3a6bfc3fa62a02088da8f575749586234379f
ProPratik1405 <sariapratik@gmail.com>
2022-03-16 16:43:58 +0530

    SPL Added

--- programs/escrow-mod/src/lib.rs
+++ programs/escrow-mod/src/lib.rs
@@ -1,7 +1,8 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::program::invoke;
 use anchor_lang::solana_program::system_instruction;
-// use anchor_spl::token::{self, SetAuthority, Token, TokenAccount, Transfer};
+use anchor_spl::token::{self, Mint, SetAuthority, Token, TokenAccount, Transfer};
+use spl_token::instruction::AuthorityType;
 
 declare_id!("EtxmAK5Hio4p1NQEwrRJu6UJpheKpBf1cfg9U7x3gGEX");
 
@@ -32,11 +33,50 @@ pub mod escrow_mod {
         Ok(())
     }
 
-    // pub fn Lock_spl(ctx: Context<LockSPL>) -> Result<()> {
-    //     Ok(())
-    // }
+    pub fn lock_spl(ctx: Context<LockSPL>, amount: u64) -> Result<()> {
+        anchor_spl::token::transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.authority_token_account.to_account_info(),
+                    to: ctx.accounts.vault_account.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
 
-    pub fn claim_bounty(ctx: Context<ClaimBounty>) -> Result<()> {
+        // token::set_authority(
+        //     CpiContext::new(
+        //         ctx.accounts.token_program.to_account_info(),
+        //         token::SetAuthority {
+        //             current_authority: ctx.accounts.authority.to_account_info(),
+        //             account_or_mint: ctx.accounts.authority_token_account.to_account_info(),
+        //         },
+        //     ),
+        //     AuthorityType::AccountOwner,
+        //     Some(vault_authority),
+        // )?;
+        Ok(())
+    }
+
+    pub fn unlock_spl(ctx: Context<UnLockSPL>, amount: u64, bump: u8) -> Result<()> {
+        anchor_spl::token::transfer(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.vault_account.to_account_info(),
+                    to: ctx.accounts.winner_token_account.to_account_info(),
+                    authority: ctx.accounts.authority.to_account_info(),
+                },
+                &[&[b"vault", ctx.accounts.authority.key().as_ref(), &[bump]]],
+            ),
+            amount,
+        )?;
+        Ok(())
+    }
+
+    pub fn claim_bounty(ctx: Context<UnLockSol>) -> Result<()> {
         msg!("SHIT ITS WORKING");
         **ctx
             .accounts
@@ -73,14 +113,35 @@ pub struct LockSOL<'info> {
     pub system_program: Program<'info, System>,
 }
 
-// #[derive(Accounts)]
-// pub struct LockSPL<'info> {
-//     pub system_program: Program<'info, System>,
-//     pub token_program: Program<'info, Token>,
-// }
+#[derive(Accounts)]
+pub struct LockSPL<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(init_if_needed, payer = authority,seeds=[b"vault",authority.key().as_ref()],bump,token::mint=mint,token::authority=authority)]
+    pub vault_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub authority_token_account: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>,
+}
 
 #[derive(Accounts)]
-pub struct ClaimBounty<'info> {
+pub struct UnLockSPL<'info> {
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(mut,seeds=[b"vault",authority.key().as_ref()],bump)]
+    pub vault_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub winner_token_account: Account<'info, TokenAccount>,
+    pub mint: Account<'info, Mint>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct UnLockSol<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
     #[account(mut,seeds=[b"bounty",authority.key().as_ref()],bump=bounty_account.bump,has_one=authority)]
@@ -94,6 +155,15 @@ pub struct ClaimBounty<'info> {
 #[derive(Default)]
 pub struct BountyAccount {
     pub authority: Pubkey,
+    pub amount: u64,
+    pub bump: u8,
+}
+
+#[account]
+#[derive(Default)]
+pub struct BountySPLAccount {
+    pub authority: Pubkey,
+    pub authority_token_account: Pubkey,
     pub amount: u64,
     pub bump: u8,
 }
--- tests/escrow-mod.ts
+++ tests/escrow-mod.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { EscrowMod } from "../target/types/escrow_mod";
+import {TOKEN_PROGRAM_ID,mintTo,transfer,createMint,getOrCreateAssociatedTokenAccount,mintToChecked,getMint, MINT_SIZE, createInitializeMintInstruction, getAssociatedTokenAddress, createAssociatedTokenAccountInstruction, getAccount,createMintToCheckedInstruction} from "@solana/spl-token"
+
 
 describe("escrow-mod", () => {
   // Configure the client to use the local cluster.
@@ -47,4 +49,109 @@ describe("escrow-mod", () => {
 
     console.log("TX Done",tx);
   })
+  it("SHould be able to transfer SPL",async() => {
+
+    const lamports = await program.provider.connection.getMinimumBalanceForRentExemption(MINT_SIZE);
+    const mint = anchor.web3.Keypair.generate();
+
+    const winner_account = anchor.web3.Keypair.generate();
+
+    let winner_ata = await getAssociatedTokenAddress(
+      mint.publicKey, // mint
+      winner_account.publicKey
+    )
+
+    let ata = await getAssociatedTokenAddress(
+      mint.publicKey, // mint
+      program.provider.wallet.publicKey // owner
+    );
+    console.log(`ATA: ${ata.toBase58()}`);
+
+    let tx = new anchor.web3.Transaction().add(
+      // create mint account
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: program.provider.wallet.publicKey,
+        newAccountPubkey: mint.publicKey,
+        space: MINT_SIZE,
+        lamports: lamports,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      // init mint account
+      createInitializeMintInstruction(
+        mint.publicKey, // mint pubkey
+        8, // decimals
+        program.provider.wallet.publicKey, // mint authority
+        program.provider.wallet.publicKey, // freeze authority (you can use `null` to disable it. when you disable it, you can't turn it on again)
+      ),
+      createAssociatedTokenAccountInstruction(
+        program.provider.wallet.publicKey,
+        ata,
+        program.provider.wallet.publicKey,
+        mint.publicKey
+      ),
+      createAssociatedTokenAccountInstruction(
+        program.provider.wallet.publicKey,
+        winner_ata,
+        winner_account.publicKey,
+        mint.publicKey
+      )
+    );
+
+    const res = await program.provider.send(tx,[mint]);
+
+    let new_tx = new anchor.web3.Transaction().add(
+      createMintToCheckedInstruction(
+        mint.publicKey, // mint
+        ata, // receiver (sholud be a token account)
+        program.provider.wallet.publicKey, // mint authority
+        10000e8, // amount. if your decimals is 8, you mint 10^8 for 1 token.
+        8 // decimals
+      )
+    )
+
+    const res_mint = await program.provider.send(new_tx);
+
+    console.log("MINT TX",res_mint);
+
+    let mintAccount = await getMint(program.provider.connection, mint.publicKey);
+
+    console.log("MINT ACCCOUNT INFO",mintAccount);
+
+    let [vault, vault_bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("vault"),program.provider.wallet.publicKey.toBuffer()],
+      program.programId
+    );
+
+    console.log("VAULT ACCOUNT",vault.toString());
+
+    const ptx = await program.rpc.lockSpl(new anchor.BN(10000000000),{accounts:{
+      authority: program.provider.wallet.publicKey,
+      authorityTokenAccount:ata,
+      mint:mint.publicKey,
+      systemProgram:anchor.web3.SystemProgram.programId,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      vaultAccount:vault,
+      rent: anchor.web3.SYSVAR_RENT_PUBKEY
+    }})
+
+    console.log("FINAL TX",ptx);
+
+    console.log("WINNER ACCOUNT",winner_ata.toString());
+
+    const withdraw = await program.rpc.unlockSpl(new anchor.BN(1000000000),vault_bump,{
+      accounts:{
+        authority: program.provider.wallet.publicKey,
+        mint:mint.publicKey,
+        systemProgram:anchor.web3.SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        vaultAccount:vault,
+        winnerTokenAccount:winner_ata
+      }
+    })
+
+    console.log("WITH DRAW TX",withdraw);
+  })
 });
+
+
+
